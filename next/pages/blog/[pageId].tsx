af72173298d61e174b76e8cbf8a5d675a6fe6fc6
Kathryn Gonzalez <kathrynmichellegonzalez@gmail.com>
2023-07-26 13:21:51 +0800

    feat: add initial related-posts functionality

--- next/pages/blog/[pageId].tsx
+++ next/pages/blog/[pageId].tsx
@@ -1,6 +1,18 @@
 import * as React from "react"
 import { GetStaticPaths, GetStaticPropsContext } from "next"
+import Image from "next/image"
+import Link from "next/link"
 import { ExtendedRecordMap } from "notion-types"
+import {
+  NotionDateTime,
+  estimatePageReadTimeAsHumanizedString,
+  formatDate,
+  formatNotionDateTime,
+  getCanonicalPageId,
+  getDateValue,
+  getPageProperty,
+  getPageTitle,
+} from "notion-utils"
 
 import {
   isDev,
@@ -8,16 +20,112 @@ import {
   rootDomain,
   rootNotionPageId,
 } from "@/config/notion"
+import { siteConfig } from "@/config/site"
 import * as notion from "@/lib/notion"
-import { resolveNotionPage } from "@/lib/resolve-notion-page"
+import {
+  ResolveNotionPageResult,
+  resolveNotionPage,
+} from "@/lib/resolve-notion-page"
+import { Block } from "@/lib/types"
 import { NotionPage } from "@/components/notion/NotionPage"
+
+interface Post {
+  pageId: string
+  recordMap: ExtendedRecordMap
+}
+
+interface RelatedPosts {
+  [key: string]: Post
+}
+
+function RelatedPost({ post }: { post: Post }) {
+  const { pageId, recordMap } = post
+  const date = getPageProperty(
+    "Published Date",
+    recordMap?.block[pageId]?.value as Block,
+    recordMap as ExtendedRecordMap
+  )
+  return (
+    <li>
+      <Link
+        href={`/blog/${getCanonicalPageId(pageId, recordMap, {
+          uuid: false,
+        })}`}
+        className="group w-full hover:bg-secondary block p-2 rounded-lg transition-colors duration-200"
+      >
+        <span className="font-bold block group-hover:underline">
+          {getPageTitle(recordMap as ExtendedRecordMap)}
+        </span>
+        <span className="text-sm w-full">{formatDate(date as string)}</span>
+      </Link>
+    </li>
+  )
+}
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const rawPageId = context?.params?.pageId as string
-  const props = await resolveNotionPage("ryngonzalez.com", rawPageId)
+  const props = (await resolveNotionPage(
+    "ryngonzalez.com",
+    rawPageId
+  )) as ResolveNotionPageResult
+
+  const pageTags = getPageProperty(
+    "Tags",
+    props.recordMap.block[props.pageId]?.value,
+    props.recordMap
+  )
+
+  async function getRelatedPosts(): Promise<RelatedPosts | undefined> {
+    try {
+      const results = await notion.getSiteMap()
+      const published = Object.entries(results?.pageMap)?.filter(
+        ([pageId, recordMap]) => {
+          return getPageProperty(
+            "Published",
+            recordMap?.block[pageId]?.value as Block,
+            recordMap as ExtendedRecordMap
+          )
+        }
+      )
+      const publishedAndRelated = published
+        .filter(([pageId, recordMap]) => {
+          const tags = getPageProperty(
+            "Tags",
+            recordMap?.block[pageId]?.value as Block,
+            recordMap as ExtendedRecordMap
+          ) as string[]
+          return tags?.some((tag) => (pageTags as string[])?.includes(tag))
+        })
+        .filter(([pageId]) => pageId !== props.pageId)
+      return publishedAndRelated
+        .slice(0, 4)
+        .reduce((acc, [pageId, recordMap]) => {
+          return {
+            ...acc,
+            [pageId]: {
+              pageId,
+              recordMap,
+            },
+          }
+        }, {})
+    } catch (e: any) {
+      console.log(e.message)
+    }
+  }
+
+  const relatedPosts = await getRelatedPosts()
 
   return {
-    props,
+    props: {
+      ...props,
+      estimatedReadTime: estimatePageReadTimeAsHumanizedString(
+        props.recordMap?.block[props.pageId].value as Block,
+        props.recordMap as ExtendedRecordMap,
+        {}
+      ),
+      tags: pageTags,
+      relatedPosts,
+    },
     // we will attempt to re-generate the page:
     // - when a request comes in
     // - at most once every 10 seconds
@@ -49,13 +157,62 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 }
 
-export default function Page({ recordMap }: { recordMap: ExtendedRecordMap }) {
+export default function Page({
+  recordMap,
+  pageId,
+  relatedPosts: relatedPosts,
+}: {
+  recordMap: ExtendedRecordMap
+  pageId: string
+  relatedPosts: RelatedPosts
+}) {
+  let relatedPostsFooter = null
+  let hasRelatedPosts = false
+  if (relatedPosts as RelatedPosts) {
+    const posts = Object.values(relatedPosts)
+    relatedPostsFooter = posts.map((post) => (
+      <RelatedPost post={post} key={post.pageId} />
+    ))
+    hasRelatedPosts = posts.length > 0
+  }
+
   return (
     <NotionPage
       recordMap={recordMap}
       rootDomain={rootDomain}
       rootPageId={rootNotionPageId}
       previewImagesEnabled={previewImagesEnabled}
+      pageFooter={
+        <div className="w-full pt-8">
+          <div className="border-t border-border pt-8">
+            <div className="flex gap-6 items-center">
+              <Image
+                alt="Profile picture of Kathryn Gonzalez"
+                src="/stickers/headshot.jpg"
+                width={96}
+                height={96}
+                className="rounded-full border-4 border-background drop-shadow-lg"
+              />
+              <div>
+                <span className="font-bold text-2xl">Kathryn Gonzalez</span>
+                <span className="block text-base text-secondary-foreground">
+                  {siteConfig.description}
+                </span>
+              </div>
+            </div>
+          </div>
+          {hasRelatedPosts && (
+            <div className="w-full pt-8">
+              <h2 className="text-3xl border-t border-border w-full pt-8 font-bold pb-6">
+                Related Posts
+              </h2>
+              <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2 -ml-2 -mr-2">
+                {relatedPostsFooter}
+              </ul>
+            </div>
+          )}
+        </div>
+      }
     />
   )
 }

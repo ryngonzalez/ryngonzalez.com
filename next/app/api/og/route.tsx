00f921fe092ca1ba2d854dea604513924fbc7f47
Kathryn Gonzalez <kathrynmichellegonzalez@gmail.com>
2024-03-03 23:41:54 -0800

    Another update for og metadata

--- next/app/api/og/route.tsx
+++ next/app/api/og/route.tsx
@@ -131,7 +131,7 @@ export async function GET(request: Request) {
               >
                 {hasImage && imageUrl && (
                   <img
-                    src={`${process.env.VERCEL_URL}${imageUrl}`}
+                    src={`https://${process.env.VERCEL_URL}${imageUrl}`}
                     height={280}
                     width={480}
                     style={{ objectFit: "cover", objectPosition: "top" }}

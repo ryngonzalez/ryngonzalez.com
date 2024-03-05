0c20eda738397ca0b62f848db6c2720b5957b70a
Kathryn Gonzalez <kathrynmichellegonzalez@gmail.com>
2024-03-05 15:33:03 -0800

    add precommit hook for checking empty filenames

--- next/scripts/check-filenames.js
+++ next/scripts/check-filenames.js
@@ -1,18 +1,36 @@
 const fs = require("fs")
 const path = require("path")
 
-const directoryPath = path.join(__dirname, "../public/blog/images")
-console.log(`Checking filenames in directory: ${directoryPath}`)
-fs.readdir(directoryPath, function (err, files) {
-  if (err) {
-    return console.log("Unable to scan directory: " + err)
-  }
-  files.forEach(function (file) {
-    if (file.includes(" ")) {
-      console.error(
-        `Invalid filename detected: "${file}". Filenames should not contain spaces.`
-      )
+const directoryPath = path.join(__dirname, "../public")
+
+function checkFilenames(dir) {
+  let invalidFilenames = [] // Array to store invalid filenames
+
+  fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.log(`Unable to scan directory: ${err}`)
+      process.exit(1) // Exit with error if unable to read directory
+    }
+
+    files.forEach((file) => {
+      const filePath = path.join(dir, file.name)
+      if (file.isDirectory()) {
+        checkFilenames(filePath) // Recursively check files in subdirectory
+      } else {
+        if (file.name.includes(" ")) {
+          invalidFilenames.push(filePath) // Add invalid filename to the array
+        }
+      }
+    })
+
+    if (invalidFilenames.length > 0) {
+      console.error("Invalid filenames detected:")
+      invalidFilenames.forEach((filename) => {
+        console.error(`- "${filename}". Filenames should not contain spaces.`)
+      })
       process.exit(1) // Exit with error
     }
   })
-})
+}
+
+checkFilenames(directoryPath)
